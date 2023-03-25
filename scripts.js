058a1d884d11b94c4258692eba2dc629572087cb
joydas <joy600508@gmail.com>
2023-03-25 16:20:15 +0600

    menubar complite

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const mobail = document.getElementById("mobail").addEventListener("click", () => {
+    document.getElementById("mainMenu").classList.toggle("hidden")
+})
+
+const darkMode = document.getElementById("dark").addEventListener("click",() =>{
+    document.getElementById("mainElement").classList.toggle("dark")
+})
